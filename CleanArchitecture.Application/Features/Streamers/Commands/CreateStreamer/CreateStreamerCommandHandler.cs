61c3172d6e50fe1805d91a12a9d8b20084b99244
Mariano Nicolas Vera Causich <marianonvera.22@gmail.com>
2024-01-21 21:54:23 -0300

    Capa de presentacion

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/CreateStreamer/CreateStreamerCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.CreateStreamer
+{
+    public class CreateStreamerCommandHandler : IRequestHandler<CreateStreamerCommand, int>
+    {
+        private readonly IStreamerRepository _streamerRepository;
+        private IMapper _mapper;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<CreateStreamerCommandHandler> _logger;
+
+        public CreateStreamerCommandHandler(IStreamerRepository streamerRepository, IMapper mapper, IEmailService emailService, ILogger<CreateStreamerCommandHandler> logger)
+        {
+            _streamerRepository = streamerRepository;
+            _mapper = mapper;
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(CreateStreamerCommand request, CancellationToken cancellationToken)
+        {
+            var streamerEntity = _mapper.Map<Streamer>(request);
+            var newStreamer = await _streamerRepository.AddAsync(streamerEntity);
+
+            _logger.LogInformation($"Streamer {newStreamer.Id} fue creado exitosamente.");
+
+            await SendEmail(newStreamer);
+
+            return newStreamer.Id;
+        }
+
+        private async Task SendEmail(Streamer streamer)
+        {
+            var email = new Email
+            {
+                To = "marianoveracausich@gmail.com",
+                Body = "La compania de streamer se creo correctamemte",
+                Subject = "Mensaje de alerta"
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Errores enviando el email de {streamer.Id}");
+            }
+        }
+    }
+}
